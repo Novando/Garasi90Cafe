1e0a4792f0c7be027684c70c7ceaeb1a2910bf53
Novando <rizkynovando@gmail.com>
2019-12-12 14:12:46 +0700

    Add files via upload

---
+++ perintah.js
@@ -0,0 +1,7 @@
+function bukaFormulir(){
+	document.getElementById("idMasuk").style.display = "block";
+}
+
+function tutupFormulir(){
+	document.getElementById('idMasuk').style.display = 'none';
+}
